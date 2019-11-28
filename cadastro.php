4de354a848d2146611f509c62ca26ed69f637ad1
Brenno Alcantara Milanezi <20151BSI0290@cefetes.br>
2019-11-28 09:31:41 -0300

    Alteracoes

--- cadastro.php
+++ cadastro.php
@@ -9,11 +9,11 @@
         <div class="container">
           <div class="banner_content d-md-flex justify-content-between align-items-center">
             <div class="mb-3 mb-md-0">
-              <h2 style="color: white;">Login</h2>
+              <h2 style="color: white;">Cadastro</h2>
             </div>
             <div class="page_link">
               <a href="./" style="color: white;">Início</a>
-              <a href="checkout.php" style="color: white;">Login</a>
+              <a href="./cadastro.php" style="color: white;">Cadastro</a>
             </div>
           </div>
         </div>
@@ -27,13 +27,40 @@
         <div class="billing_details">
           <div class="row">
             <div class="col-lg-12">
-              <h3></h3>
               <form class="row contact_form" action="./" method="post" novalidate="novalidate">
-                <div class="col-md-12 form-group p_star">
-                  Nome Completo<input type="text" class="form-control" id="email" name="email">
+                <div class="col-md-6 form-group p_star">
+                  <input type="text" class="form-control" id="first" name="name">
+                  <span class="placeholder" data-placeholder="Nome"></span>
+                </div>
+                <div class="col-md-6 form-group p_star">
+                  <input type="text" class="form-control" id="last" name="name">
+                  <span class="placeholder" data-placeholder="Sobrenome"></span>
+                </div>
+                <div class="col-md-6 form-group p_star">
+                  <input type="text" class="form-control" id="number" name="number">
+                  <span class="placeholder" data-placeholder="Telefone"></span>
+                </div>
+                <div class="col-md-6 form-group p_star">
+                  <input type="text" class="form-control" id="email" name="compemailany">
+                  <span class="placeholder" data-placeholder="Email"></span>
                 </div>
                 <div class="col-md-12 form-group p_star">
-                  Email<input type="text" class="form-control" id="email" name="email">
+                  <input type="text" class="form-control" id="add1" name="add1">
+                  <span class="placeholder" data-placeholder="Endereço"></span>
+                </div>
+                <div class="col-md-12 form-group p_star">
+                  <input type="text" class="form-control" id="city" name="city">
+                  <span class="placeholder" data-placeholder="Cidade"></span>
+                </div>
+                <div class="col-md-12 form-group p_star">
+                  <select class="country_select" style="display: none;">
+                    <option value="1">Espírito Santo</option>
+                    <option value="2">São Paulo</option>
+                    <option value="4">Rio de Janeiro</option>
+                  </select><div class="nice-select country_select" tabindex="0"><span class="current">Espírito Santo</span><ul class="list"><li data-value="1" class="option selected">Espírito Santo</li><li data-value="2" class="option">São Paulo</li><li data-value="4" class="option">Rio de Janeiro</li></ul></div>
+                </div>
+                <div class="col-md-12 form-group">
+                  <input type="text" class="form-control" id="zip" name="zip" placeholder="CEP">
                 </div>
                 <div class="col-md-12 form-group p_star">
                   Senha<input type="text" class="form-control" id="senha" name="senha" onkeyup="preencheCampo('valida_senha', this.value, 'validar');">
@@ -42,9 +69,6 @@
                   Confirmar Senha<input type="text" class="form-control" id="conf_senha" name="conf_senha" onkeyup="preencheCampo('valida_senha', this.value, 'validar');">
                 </div>
                 <input type="submit" name="logar" class="main_btn" value="Entrar" style="margin: auto;">
-                <div class="col-md-12 form-group p_star">
-                  <a href="./cadastro.php">Cadastrar-se</a> 
-                </div>
               </form>
             </div>
           </div>
